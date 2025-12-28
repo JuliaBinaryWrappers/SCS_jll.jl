faa2a78b566982b90ffd0b6ad25abe0d705d35fe
jlbuild <juliabuildbot@gmail.com>
2025-12-28 22:00:44 +0000

    SCS_jll build 300.200.1000+0

--- src/SCS_jll.jl
+++ src/SCS_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("SCS")
-JLLWrappers.@generate_main_file("SCS", UUID("f4f2fc5b-1d94-523c-97ea-2ab488bedf4b"))
+JLLWrappers.@generate_main_file("SCS", Base.UUID("f4f2fc5b-1d94-523c-97ea-2ab488bedf4b"))
 end  # module SCS_jll
